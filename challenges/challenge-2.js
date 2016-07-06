1e7e2bd40020144261105230360df13482adce97
Danilo Barros <danilobjr@gmail.com>
2016-07-06 17:15:23 -0300

    challenge 2

---
+++ challenges/challenge-2.js
@@ -0,0 +1,15 @@
+// [
+//     {
+//         "id": 70111470,
+//         "title": "Die Hard",
+//         "boxart": "http://cdn-0.nflximg.com/images/2891/DieHard150.jpg"
+//     },
+//     {
+//         "id": 654356453,
+//         "title": "Bad Boys",
+//         "boxart": "http://cdn-0.nflximg.com/images/2891/DieHard150.jpg"
+//     }
+// ]
+
+// filter, map, concatAll
+// CANT USE: boxarts[0] .. boxarts.pop() .shift()
