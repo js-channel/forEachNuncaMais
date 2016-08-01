3c3e8f8b75ad6559449cb830c885861875b3079a
Danilo Barros <danilobjr@gmail.com>
2016-08-01 18:01:30 -0300

    update comments

--- challenges/challenge-1.js
+++ challenges/challenge-1.js
@@ -1,8 +1,9 @@
 const { movies } = require('./filter/filterData');
 const { log } = require('./helpers');
 
-// This is the resolution for filter exercises
+// This is the resolution for filter() exercises
 // If you want to try it, check this link: jsbin.com/vivosej
+// Resolution in this video: https://youtu.be/H8ginSxcTbU?list=PLhxF6V44XvXQzeZqn3Xd95RKm_Gr7JT7y 
 
 const propEquals = propName => value => obj => 
     obj[propName] === value
