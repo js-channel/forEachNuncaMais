20d88e36c831a148b04b1796433ed6976d3e9e78
Danilo Barros <danilobjr@gmail.com>
2016-08-01 17:10:48 -0300

    zip extension

--- extensions.js
+++ extensions.js
@@ -11,3 +11,13 @@ Array.prototype.concatAll = function() {
 Array.prototype.concatMap = function (modifierFunction) {
     return this.map(modifierFunction).concatAll();
 }
+
+Array.zip = function(xs, ys, modify) {
+    const result = [];
+
+    for(let i = 0; i < Math.min(xs.length, ys.length); i++) {
+        result.push(modify(xs[i], ys[i]));
+    }
+
+    return result;
+}
